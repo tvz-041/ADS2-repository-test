d23a9347114e1587aab9f38203f5c17fcb1ab3f2
tvz <tvz.041@gmail.com>
2022-03-02 14:06:04 +0700

    Создан проект двоичных деревьев, реализовано добавление и вывод

---
+++ Trees/Trees/BinaryTree.cpp
@@ -0,0 +1,86 @@
+#include <random>
+#include <iostream>
+
+#include "BinaryTree.h"
+
+Node *BinaryTree::getRoot()
+{
+	return m_root;
+}
+
+Node *BinaryTree::addNode(Node *subTreeRoot, const int key)
+{
+	if (subTreeRoot == nullptr) {
+		if (m_root == nullptr) {
+			m_root = new Node(key);
+			return m_root;
+		} else {
+			return nullptr;
+		}
+	}
+
+	if (subTreeRoot->leftChild == nullptr) {
+		subTreeRoot->leftChild = new Node(key);
+		return subTreeRoot->leftChild;
+	} else if (subTreeRoot->rightChild == nullptr) {
+		subTreeRoot->rightChild = new Node(key);
+		return subTreeRoot->rightChild;
+	} else {
+		if (rand() % 2) {
+			return addNode(subTreeRoot->leftChild, key);
+		} else {
+			return addNode(subTreeRoot->rightChild, key);
+		}
+	}
+}
+
+void BinaryTree::printHorizontal()
+{
+	printHorizontal(m_root);
+}
+
+void BinaryTree::printHorizontal(Node *subTreeRoot, const int level)
+{
+	using std::cout;
+	using std::endl;
+
+	if (subTreeRoot == nullptr) {
+		if (subTreeRoot == m_root) {
+			cout << "Tree is empty" << endl;
+		}
+		return;
+	}
+
+	printHorizontal(subTreeRoot->rightChild, level + 1);
+
+	for (int i = 0; i < level; i++)
+		cout << "   ";
+	cout << subTreeRoot->key << endl;
+	
+	printHorizontal(subTreeRoot->leftChild, level + 1);
+}
+
+void BinaryTree::printLevel(const int level)
+{
+	printLevel(m_root, level, 0);
+}
+
+void BinaryTree::printLevel(Node * subTreeRoot, const int level, const int currentLevel)
+{
+	using std::cout;
+	using std::endl;
+
+	if (subTreeRoot == nullptr) {
+		if (subTreeRoot == m_root) {
+			cout << "Tree is empty" << endl;
+		}
+		return;
+	}
+
+	if (currentLevel == level) {
+		cout << subTreeRoot->key << "   ";
+	} else if (currentLevel < level) {
+		printLevel(subTreeRoot->leftChild, level, currentLevel + 1);
+		printLevel(subTreeRoot->rightChild, level, currentLevel + 1);
+	}
+}
---
+++ Trees/Trees/BinaryTree.h
@@ -0,0 +1,34 @@
+#pragma once
+
+struct Node
+{
+	Node(const int key = 13, Node *leftChild = nullptr, Node *rightChild = nullptr) :
+		key(key),
+		leftChild(leftChild),
+		rightChild(rightChild)
+	{}
+
+	int key = 10;
+	Node *leftChild = nullptr;
+	Node *rightChild = nullptr;
+};
+
+class BinaryTree
+{
+public:
+	BinaryTree() = default;
+
+	Node *getRoot();
+	Node *addNode(Node *subTreeRoot, const int key);
+
+	void printHorizontal();
+	void printHorizontal(Node *subTreeRoot, const int level = 0);
+
+	void printLevel(const int level);
+	void printLevel(Node *subTreeRoot, const int level, const int currentLevel = 0);
+
+private:
+	Node *m_root = nullptr;
+};
+
+
